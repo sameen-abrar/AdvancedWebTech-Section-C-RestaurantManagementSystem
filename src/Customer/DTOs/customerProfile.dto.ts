f016a7f63685071df4e7b83b95460636b0c82287
notactually-me <sameenabrar13@gmail.com>
2023-02-13 01:17:46 +0600

    validation pipes

--- src/Customer/DTOs/customerProfile.dto.ts
+++ src/Customer/DTOs/customerProfile.dto.ts
@@ -2,16 +2,16 @@ import { IsInt, IsNotEmpty, isNotEmpty, isNumber, IsString, Length } from "class
 
 export class UserDTO
 {
-    @IsInt()
-    @IsNotEmpty()
+    @IsInt({message: "ID must be integer"})
+    @IsNotEmpty({message: "cannot enter without valid ID"})
     id: number;
 
-    @IsString()
-    @IsNotEmpty()
-    @Length(3,50)
+    @IsString({message: "Please provide string type for name"})
+    @IsNotEmpty({message: "Name is required"})
+    @Length(3,50, {message: "Name shoudl be more than 3 letters"} )
     name: string;
 
-    @IsInt()
-    @IsNotEmpty()
+    @IsInt({message: "Should be a valid number"})
+    @IsNotEmpty({message: "Phone number is required for verification"})
     phone: number;
 }
